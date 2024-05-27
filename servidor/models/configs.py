8ac3b61763210872b2e61b33b3f880d75157b779
Victor Hugo Carvalho Dos Reis Santos <admin@katomaro.com>
2024-05-27 01:48:10 -0300

    tdd[pass]: config do katomart

--- servidor/models/configs.py
+++ servidor/models/configs.py
@@ -1,1 +1,12 @@
-print('I am a little teapot, short and stout. Here is my handle, here is my spout.')
+from . import Base
+from sqlalchemy import Column, Integer, String, Boolean
+
+class Configuracao(Base):
+    __tablename__ = 'configs'
+    id = Column(Integer, primary_key=True)
+    chave = Column(String(100), nullable=False, unique=True)
+    valor = Column(String(255))
+    ativo = Column(Boolean, default=True)
+
+    def __repr__(self):
+        return f"<Configuracao(chave='{self.chave}', valor='{self.valor}', ativo={self.ativo})>"
