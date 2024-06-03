ff066066088bda6a69cd119763d7909369a6d886
Victor Hugo Carvalho Dos Reis Santos <admin@katomaro.com>
2024-06-03 01:56:45 -0300

    chore[setup]: add a check for geckodriver (boycott manifest v3)

--- setup_utils.py
+++ setup_utils.py
@@ -2,3 +2,6 @@ import shutil
 
 def check_for_ffmpeg() -> bool:
     return shutil.which("ffmpeg") is not None
+
+def check_geckodriver() -> bool:
+    return shutil.which("geckodriver") is not None
