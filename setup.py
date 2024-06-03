fda2becce95911c1d7e5730686ac0ec426a43221
Victor Hugo Carvalho Dos Reis Santos <admin@katomaro.com>
2024-06-03 02:54:00 -0300

    chore[setup]: add colorama autoreset

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 import colorama
 
+
+colorama.init(autoreset=True)
+
 print(colorama.Fore.CYAN + 'Boas vindas ao utilitário de instalação do Katomart!\n\n' + colorama.Style.RESET_ALL
       + 'Este utilitário irá verificar se você possui as ferramentas de sistema necessárias para '
       'executar o Katomart\n\t' + colorama.Fore.YELLOW + 'Caso você não possua alguma das ferramentas, '
