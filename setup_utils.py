fc949a5866b7c1195d091aeba47d00eb450081ad
Victor Hugo Carvalho Dos Reis Santos <admin@katomaro.com>
2024-06-03 02:00:57 -0300

    refactor[setup]: DRY - join all cli tool checks

--- setup_utils.py
+++ setup_utils.py
@@ -1,7 +1,5 @@
 import shutil
 
-def check_for_ffmpeg() -> bool:
-    return shutil.which("ffmpeg") is not None
-
-def check_geckodriver() -> bool:
-    return shutil.which("geckodriver") is not None
+def check_for_cli_tool(tool_name: str=''):
+    """Check if a CLI tool is installed"""
+    return shutil.which(tool_name) is not None
