b013ee6c56506486886431103ad68031d67d2091
Victor Hugo Carvalho Dos Reis Santos <admin@katomaro.com>
2024-06-03 02:44:41 -0300

    chore[setup]: add a function to check if the user can run the software at all

--- setup_utils.py
+++ setup_utils.py
@@ -1,6 +1,10 @@
 import shutil
 import sys
 
+def check_python_support(major: int=3, minor: int=12) -> bool:
+    """Check if the Python version is supported"""
+    return sys.version_info.major == major and sys.version_info.minor >= minor
+
 def check_for_cli_tool(tool_name: str=''):
     """Check if a CLI tool is installed"""
     return shutil.which(tool_name) is not None
