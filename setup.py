ae7c9f98cb683c7a5d397aa860f926a00c5af5f4
Victor Hugo Carvalho Dos Reis Santos <admin@katomaro.com>
2024-06-03 03:34:30 -0300

    feat[setup]: check for user os

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import setup_utils
 from setup_texts import SETUP_TEXTS
 
 USER_LANGUAGE = None
+USER_OS = None
 SUPPORTED_PYTHON_VERSION = (3, 12)
 SUPPORTED_OS = ('win32', 'linux', 'darwin')
 
@@ -19,3 +20,8 @@ print(SETUP_TEXTS[USER_LANGUAGE]['check_python_version'])
 if not setup_utils.check_python_support(*SUPPORTED_PYTHON_VERSION):
     raise Exception(SETUP_TEXTS[USER_LANGUAGE]['python_version_not_supported'])
 print(SETUP_TEXTS[USER_LANGUAGE]['python_version_supported'])
+
+print(SETUP_TEXTS[USER_LANGUAGE]['check_user_os'])
+if setup_utils.get_operating_system() not in SUPPORTED_OS:
+    raise Exception(SETUP_TEXTS[USER_LANGUAGE]['unsupported_os'])
+print(SETUP_TEXTS[USER_LANGUAGE]['supported_os'])
