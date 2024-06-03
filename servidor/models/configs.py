809b153ada82dc3716bf11b41398d103a099228c
Victor Hugo Carvalho Dos Reis Santos <admin@katomaro.com>
2024-06-03 19:43:06 -0300

    fix[models]: config value limit

--- servidor/models/configs.py
+++ servidor/models/configs.py
@@ -1,11 +1,11 @@
 from . import Base
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, Text
 
 class Configuration(Base):
     __tablename__ = 'configs'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     key = Column(String(100), nullable=False, unique=True)
-    value = Column(String(255))
+    value = Column(Text, nullable=False)
     enabled = Column(Boolean, default=True)
 
     def __repr__(self):
