e8c4f382b65eaac28f4d311629623caf58ae412c
Victor Hugo Carvalho Dos Reis Santos <admin@katomaro.com>
2024-06-03 02:50:51 -0300

    chore[setup]: add a intro text

---
+++ setup.py
@@ -0,0 +1,8 @@
+import colorama
+
+print(colorama.Fore.CYAN + 'Boas vindas ao utilitário de instalação do Katomart!\n\n' + colorama.Style.RESET_ALL
+      + 'Este utilitário irá verificar se você possui as ferramentas de sistema necessárias para '
+      'executar o Katomart\n\t' + colorama.Fore.YELLOW + 'Caso você não possua alguma das ferramentas, '
+      'o utilitário irá te explicar sua necessidade e perguntar se você deseja instalar a ferramenta.\n\n'
+      + colorama.Style.RESET_ALL + 'Nem todas as ferramentas são necessárias, mas, melhoram a experiência!')
+
