d30ecae6fcb60730913c80e99e1a8ab25fc4aeba
Victor Hugo Carvalho Dos Reis Santos <admin@katomaro.com>
2024-06-03 02:40:36 -0300

    chore[setup]: add function to get the user operating system

--- setup_utils.py
+++ setup_utils.py
@@ -1,5 +1,11 @@
 import shutil
+import sys
 
 def check_for_cli_tool(tool_name: str=''):
     """Check if a CLI tool is installed"""
     return shutil.which(tool_name) is not None
+
+def get_operating_system() -> str:
+    """Get the operating system name"""
+    operating_system = sys.platform
+    return operating_system if operating_system in ['linux', 'win32', 'darwin'] else 'unsupported'
