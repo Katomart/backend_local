ec534a5f94248b17307c5974723dff1bf0e9fbd9
Victor Hugo Carvalho Dos Reis Santos <admin@katomaro.com>
2024-06-03 20:02:23 -0300

    feat[setup]: add default config: home addresses

--- servidor/config.py
+++ servidor/config.py
@@ -13,9 +13,16 @@ OPERATING_SYSTEM = get_operating_system()
 def set_default_config() -> None:
     """Set the default application configuration
     """
+    # Path Stuff
     download_conf, char_conf = set_download_path()
     db_session.add(download_conf)
     db_session.add(char_conf)
+
+    # Home Stuff
+    home_conf, repo_conf = set_home_address()
+    db_session.add(home_conf)
+    db_session.add(repo_conf)
+
     db_session.commit()
 
 def set_download_path(download_path: pathlib.Path=pathlib.Path(SERVER_PATH),
@@ -24,6 +31,8 @@ def set_download_path(download_path: pathlib.Path=pathlib.Path(SERVER_PATH),
     Params:
         download_path (pathlib.Path): The path to download the files to
         user_os (str): The operating system name
+    Returns:
+        tuple[Configuration, Configuration]: The download path and the remaining path length
     """
     download_path = pathlib.Path(download_path) \
     .resolve() \
@@ -34,7 +43,19 @@ def set_download_path(download_path: pathlib.Path=pathlib.Path(SERVER_PATH),
     remaining = remaining_path_length(pathlib.Path(download_path), user_os)
 
     download_conf = Configuration(key='download_path', value=download_path)
-    char_conf = Configuration(key='remaining_characters', value=remaining)
+    char_conf = Configuration(key='remaining_save_length', value=remaining)
 
     return download_conf, char_conf
 
+def set_home_address(home_address: str='katomart.com',
+                      repo_url: str='https://github.com/katomaro/katomart') -> tuple[Configuration, Configuration]:
+    """Set the home address for the application
+    Params:
+        home_address (str): The adress to which the application should phone to
+        repo_url (str): The URL of the repository
+    Returns:
+        tuple[Configuration, Configuration]: The home address and the repository URL
+    """
+    home_conf = Configuration(key='home_address', value=home_address)
+    repo_conf = Configuration(key='repository_url', value=repo_url)
+    return home_conf, repo_conf
