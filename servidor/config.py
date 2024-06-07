eb0ee9df630ca6a683cf362a9f472fe076a0c24a
Victor Hugo Carvalho Dos Reis Santos <admin@katomaro.com>
2024-06-07 03:25:32 -0300

    feat[config]: add default configs

--- servidor/config.py
+++ servidor/config.py
@@ -6,6 +6,8 @@ from servidor import db_session
 
 from models.configs import Configuration
 
+from sec import generate_hwid
+
 
 SERVER_PATH = get_execution_path()
 OPERATING_SYSTEM = get_operating_system()
@@ -34,6 +36,107 @@ def set_default_config() -> None:
     db_session.add(home_conf)
     db_session.add(repo_conf)
     db_session.add(dev_conf)
+
+    # DEFAULT CONFIGURATION
+    set_config_from_setup()
+
+    user_local_consent = Configuration(key='user_local_consent', value='False')
+    db_session.add(user_local_consent)
+
+    last_execution = Configuration(key='last_execution', value='0')
+    db_session.add(last_execution)
+
+    enable_local_katomart_api = Configuration(key='enable_local_katomart_api', value='False')
+    db_session.add(enable_local_katomart_api)
+
+    enable_local_katomart_user_registration = Configuration(key='enable_local_katomart_user_registration', value='False')
+    db_session.add(enable_local_katomart_user_registration)
+
+    use_original_media_name = Configuration(key='use_original_media_name', value='False')
+    db_session.add(use_original_media_name)
+
+    download_subtitles = Configuration(key='download_subtitles', value='False')
+    db_session.add(download_subtitles)
+
+    subtitle_language = Configuration(key='subtitle_language', value='pt')
+    db_session.add(subtitle_language)
+
+    download_quality = Configuration(key='download_quality', value='1080p')
+    db_session.add(download_quality)
+
+    download_quality_fallback = Configuration(key='download_quality_fallback', value='best')
+    db_session.add(download_quality_fallback)
+
+    download_format = Configuration(key='download_format', value='mp4')
+    db_session.add(download_format)
+
+    download_threads = Configuration(key='download_threads', value='1')
+    db_session.add(download_threads)
+
+    stream_threads = Configuration(key='stream_threads', value='5')
+    db_session.add(stream_threads)
+
+    download_maximum_retries_per_file = Configuration(key='download_maximum_retries_per_file', value='5')
+    db_session.add(download_maximum_retries_per_file)
+
+    download_await_time = Configuration(key='download_await_time', value='5')
+    db_session.add(download_await_time)
+
+    download_await_on_fail = Configuration(key='download_await_on_fail', value='5')
+    db_session.add(download_await_on_fail)
+
+    reauth_on_fail = Configuration(key='reauth_on_fail', value='False')
+    db_session.add(reauth_on_fail)
+
+    reauth_fail_threshold = Configuration(key='reauth_fail_threshold', value='3')
+    db_session.add(reauth_fail_threshold)
+
+    reauth_await_time = Configuration(key='reauth_await_time', value='180')
+    db_session.add(reauth_await_time)
+
+    reauth_maximum_retries = Configuration(key='reauth_maximum_retries', value='2')
+    db_session.add(reauth_maximum_retries)
+
+    download_stream_segment_in_order = Configuration(key='download_stream_segment_in_order', value='True')
+    db_session.add(download_stream_segment_in_order)
+
+    default_user_agent = Configuration(key='default_user_agent', value='Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:122.0) Gecko/20100101 Firefox/122.0')
+    db_session.add(default_user_agent)
+
+    download_from_widevine = Configuration(key='download_from_widevine', value='False')
+    db_session.add(download_from_widevine)
+
+    cdm_path = Configuration(key='cdm_path', value='')
+    db_session.add(cdm_path)
+
+    downloaded_videos_from_html_files = Configuration(key='downloaded_videos_from_html_files', value='False')
+    db_session.add(downloaded_videos_from_html_files)
+
+    auth_threads = Configuration(key='auth_threads', value='5')
+    db_session.add(auth_threads)
+
+
+    # API CONFIGURATION
+    allow_remote_api_communication = Configuration(key='allow_remote_api_communication', value='False')
+    db_session.add(allow_remote_api_communication)
+
+    unhashed_hwid, hashed_hwid = generate_hwid()
+    public_hwid = Configuration(key='public_hwid', value=hashed_hwid)
+    db_session.add(public_hwid)
+    registration_uuid = Configuration(key='registration_uuid', value=unhashed_hwid)
+    db_session.add(registration_uuid)
+    public_pgp_key = Configuration(key='public_pgp_key', value='Cadastrar')
+    db_session.add(public_pgp_key)
+    api_username = Configuration(key='api_username', value='Cadastrar')
+    db_session.add(api_username)
+    api_password = Configuration(key='api_password', value='Cadastrar')
+    db_session.add(api_password)
+    api_token = Configuration(key='api_token', value='Cadastrar')
+    db_session.add(api_token)
+    api_consent = Configuration(key='api_consent', value='False')
+    db_session.add(api_consent)
+    api_consent_date = Configuration(key='api_consent_date', value='0')
+    db_session.add(api_consent_date)
 
     db_session.commit()
 
