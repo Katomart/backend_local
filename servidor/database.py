74aa9aed4a1012431ccc18084b98c82af836fe5f
Victor Hugo Carvalho Dos Reis Santos <admin@katomaro.com>
2024-06-13 18:29:49 -0300

    fix[pytest]: wrong database initialization

--- servidor/database.py
+++ servidor/database.py
@@ -7,6 +7,7 @@ db_session = scoped_session(sessionmaker())
 def init_db(app):
     global db_session
     engine = create_engine(app.config['DATABASE_URL'], echo=True, connect_args={"check_same_thread": False})
+    db_session.remove()
     db_session.configure(bind=engine)
     Base.metadata.bind = engine
     Base.metadata.create_all(engine)
