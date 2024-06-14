c465f171b139930be7c169212708d3e2d117ebd2
Victor Hugo Carvalho Dos Reis Santos <admin@katomaro.com>
2024-06-14 15:33:02 -0300

    feat[MAIN]: add endpoint to get all accounts

--- servidor/api.py
+++ servidor/api.py
@@ -1,10 +1,19 @@
 from flask import jsonify
 
+from .auth import requires_token, requires_consent
+from .database import db_session
+
+from .models.courses import PlatformAuth, Platform, Course, Module, Lesson, File
+
 def setup_api_routes(api_blueprint):
     @api_blueprint.route('/ping')
     def ping():
         return jsonify({'message': 'pong'})
 
-    @api_blueprint.route('/user_content', methods=['GET'])
-    def user_content():
-        return jsonify({'message': 'user content'})
+    @requires_consent
+    @requires_token
+    @api_blueprint.route('/get_all_accounts')
+    def get_all_accounts():
+        all_accounts = PlatformAuth.query.all()
+        return jsonify([account.to_dict() for account in all_accounts]), 200
+    
