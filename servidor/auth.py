fe745245de055b9aaa58a5f344a745c953d2c1f0
Victor Hugo Carvalho Dos Reis Santos <admin@katomaro.com>
2024-06-14 13:44:42 -0300

    feat[MAIN]: add a decorator for api ToS consent check

--- servidor/auth.py
+++ servidor/auth.py
@@ -1,6 +1,10 @@
 from functools import wraps
 from flask import request, jsonify
 
+from sqlalchemy.orm import Session
+from .database import db_session
+from .models.configs import Configuration
+
 def token_is_valid(token):
     # Placeholder
     return token == "katomart"
@@ -21,4 +25,19 @@ def protected_request(f):
             return jsonify({'message': 'Invalid or expired Token.'}), 403
 
         return f(*args, **kwargs)
+    return decorated_function
+
+def requires_consent(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        session = Session(bind=db_session.get_bind())
+        
+        consent = session.query(Configuration).filter_by(key='user_local_consent').first()
+        session.close()
+
+        if consent and consent.enabled and consent.value.lower() == 'true':
+            return f(*args, **kwargs)
+        else:
+            return jsonify({'message': 'DENIED. You must consent to the ToS to use this application.'}), 403
+
     return decorated_function
