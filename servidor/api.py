f96c4a04e6e6faf3f519a8c4544cfce8896ee085
Victor Hugo Carvalho Dos Reis Santos <admin@katomaro.com>
2024-06-15 18:32:43 -0300

    fix[MAIN/API]: fixes decorator uses

--- servidor/api.py
+++ servidor/api.py
@@ -9,21 +9,22 @@ def setup_api_routes(api_blueprint):
     def ping():
         return jsonify({'message': 'pong'}), 200
 
-    @requires_consent
-    @requires_token
+
     @api_blueprint.route('/get_all_accounts')
+    @requires_token
+    @requires_consent
     def get_all_accounts():
         all_accounts = g.session.query(PlatformAuth).all()
         return jsonify([account.to_dict() for account in all_accounts]), 200
     
-    @requires_consent
     @api_blueprint.route('/get_all_platforms')
+    @requires_consent
     def get_all_platforms():
         all_platforms = g.session.query(Platform).all()
         return jsonify([platform.to_dict() for platform in all_platforms]), 200
     
-    @requires_consent
     @api_blueprint.route('/get_all_courses')
+    @requires_consent
     def get_all_courses():
         all_courses = g.session.query(Course).all()
         return jsonify([course.to_dict() for course in all_courses]), 200
