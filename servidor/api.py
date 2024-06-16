1fd81f361d9a99a6092c5a947d2a833d48cac6dc
Victor Hugo Carvalho Dos Reis Santos <admin@katomaro.com>
2024-06-16 19:31:52 -0300

    feat[MAIN/API]: basic routes for getting course data

--- servidor/api.py
+++ servidor/api.py
@@ -24,21 +24,81 @@ def setup_api_routes(api_blueprint):
         return jsonify({'message': 'pong'}), 200
 
 
-    @api_blueprint.route('/get_all_accounts')
+    @api_blueprint.route('/get_all_accounts', methods=['GET'])
     @requires_token
     @requires_consent
     def get_all_accounts():
         all_accounts = g.session.query(PlatformAuth).all()
+        if all_accounts is None:
+            return jsonify({'message': 'No accounts found'}), 404
         return jsonify([account.to_dict() for account in all_accounts]), 200
     
-    @api_blueprint.route('/get_all_platforms')
+    @requires_token
+    @requires_consent
+    @api_blueprint.route('/platform_accounts/<int:id>', methods=['GET'])
+    def get_account(id):
+        auth = g.session.query(PlatformAuth).get(id)
+        if auth is None:
+            return jsonify({'message': 'Conta não encontrada'}), 404
+        return jsonify(auth.to_dict()), 200
+
+
+    @api_blueprint.route('/get_all_platforms', methods=['GET'])
     @requires_consent
     def get_all_platforms():
         all_platforms = g.session.query(Platform).all()
+        if all_platforms is None:
+            return jsonify({'message': 'No platforms found'}), 404
         return jsonify([platform.to_dict() for platform in all_platforms]), 200
     
+    @api_blueprint.route('/platforms/<int:id>', methods=['GET'])
+    @requires_consent
+    def get_platform(id):
+        platform = g.session.query(Platform).get(id)
+        if platform is None:
+            return jsonify({'message': 'Plataforma não encontrada'}), 404
+        return jsonify(platform.to_dict()), 200
+    
+
     @api_blueprint.route('/get_all_courses')
     @requires_consent
     def get_all_courses():
         all_courses = g.session.query(Course).all()
+        if all_courses is None:
+            return jsonify({'message': 'Nenhum curso adicionado'}), 404
         return jsonify([course.to_dict() for course in all_courses]), 200
+
+    @api_blueprint.route('/courses/<int:id>', methods=['GET'])
+    @requires_consent
+    def get_course(id):
+        course = g.session.query(Course).get(id)
+        if course is None:
+            return jsonify({'message': 'Curso não encontrado'}), 404
+        return jsonify(course.to_dict()), 200
+
+
+    @api_blueprint.route('/modules/<int:id>', methods=['GET'])
+    @requires_consent
+    def get_module(id):
+        module = g.session.query(Module).get(id)
+        if module is None:
+            return jsonify({'message': 'Módulo não encontrado'}), 404
+        return jsonify(module.to_dict()), 200
+
+
+    @api_blueprint.route('/lessons/<int:id>', methods=['GET'])
+    @requires_consent
+    def get_lesson(id):
+        lesson = g.session.query(Lesson).get(id)
+        if lesson is None:
+            return jsonify({'message': 'Aula não encontrada'}), 404
+        return jsonify(lesson.to_dict()), 200
+
+
+    @api_blueprint.route('/files/<int:id>', methods=['GET'])
+    @requires_consent
+    def get_file(id):
+        file = g.session.query(File).get(id)
+        if file is None:
+            return jsonify({'message': 'Arquivo não encontrado'}), 404
+        return jsonify(file.to_dict()), 200
