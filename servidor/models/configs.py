453b29e3379a78207c60ce13678a48b3a680a981
Victor Hugo Carvalho Dos Reis Santos <admin@katomaro.com>
2024-06-23 00:42:11 -0300

    feat[main/MODELS]: add a description field to each configuration

--- servidor/models/configs.py
+++ servidor/models/configs.py
@@ -8,6 +8,7 @@ class Configuration(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     key = Column(String(100), nullable=False, unique=True)
     value = Column(Text, nullable=False)
+    description = Column(Text, nullable=True)
     enabled = Column(Boolean, default=True)
 
     def __repr__(self):
@@ -16,11 +17,14 @@ class Configuration(Base):
     def to_dict(self):
         if isinstance(self.value, bytes):
             normalized_value = self.value.decode('utf-8')
+        if isinstance(self.description, bytes):
+            normalized_description = self.description.decode('utf-8')
 
         normalized_value = json.loads(self.normalized_value)
         return {
             'id': int(self.id),
             'key': str(self.key),
             'value': normalized_value,
+            'description': normalized_description if self.description else '',
             'enabled': bool(self.enabled)
         }
