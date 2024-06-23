983546427091c818c6c65bad60966ecbdd3d11b0
Victor Hugo Carvalho Dos Reis Santos <admin@katomaro.com>
2024-06-22 22:04:17 -0300

    feat: add a script to handle installing third-party dependencies

---
+++ servidor/install.py
@@ -0,0 +1,14 @@
+import pathlib
+
+from .database import get_session
+from .models.configs import Configuration
+
+
+def try_auto_install_ffmpeg():
+    pass
+
+def try_auto_install_geckodriver():
+    pass
+
+def try_auto_install_bento4():
+    pass
