4cb07d107a03b7591b3800ff038f3af7d7710f92
Victor Hugo Carvalho Dos Reis Santos <admin@katomaro.com>
2024-06-29 13:46:10 -0300

    refactor[configs]: edita atributo padrao das configs

--- servidor/config.py
+++ servidor/config.py
@@ -59,7 +59,6 @@ def set_config_from_setup() -> None:
 
 def has_default_configs_set():
     """Check if the default configurations have already been set in the database."""
-    # Assuming there's a specific key in your Configuration table that is only added by set_default_configs
     db_session = get_session()
     config_exists = db_session.query(Configuration).filter_by(key='default_config_set').first()
     return config_exists is not None
@@ -85,158 +84,146 @@ def set_default_config() -> None:
         db_session.add(repo_conf)
         db_session.add(dev_conf)
 
-        user_local_consent = Configuration(key='user_local_consent', value='False')
+        user_local_consent = Configuration(key='user_local_consent', value='False', value_type='bool', hidden=True)
         db_session.add(user_local_consent)
 
-        user_local_consent_date = Configuration(key='user_local_consent_date', value='0')
+        user_local_consent_date = Configuration(key='user_local_consent_date', value='0', value_type='int', hidden=True)
         db_session.add(user_local_consent_date)
 
-        last_execution = Configuration(key='last_execution', value='0')
+        last_execution = Configuration(key='last_execution', value='0', value_type='int', hidden=True)
         db_session.add(last_execution)
 
-        run_in_debug_mode = Configuration(key='run_in_debug_mode', value='False')
-        db_session.add(run_in_debug_mode)
-
-        enable_local_katomart_api = Configuration(key='enable_local_katomart_api', value='False')
-        db_session.add(enable_local_katomart_api)
-
-        enable_local_katomart_user_registration = Configuration(key='enable_local_katomart_user_registration', value='False')
-        db_session.add(enable_local_katomart_user_registration)
-
-        use_original_media_name = Configuration(key='use_original_media_name', value='False')
+        use_original_media_name = Configuration(key='use_original_media_name', value='False', value_type='bool', hidden=False)
         db_session.add(use_original_media_name)
 
-        avoid_path_name_explosion_by_rooting = Configuration(key='avoid_path_name_explosion_by_rooting', value='True')
+        avoid_path_name_explosion_by_rooting = Configuration(key='avoid_path_name_explosion_by_rooting', value='True', value_type='bool', hidden=False)
         db_session.add(avoid_path_name_explosion_by_rooting)
 
-        avoid_path_name_shortening = Configuration(key='avoid_path_name_shortening', value='False')
+        avoid_path_name_shortening = Configuration(key='avoid_path_name_shortening', value='False', value_type='bool', hidden=False)
         db_session.add(avoid_path_name_shortening)
 
-        aggressive_path_name_shortening = Configuration(key='aggressive_path_name_shortening', value='False')
+        aggressive_path_name_shortening = Configuration(key='aggressive_path_name_shortening', value='False', value_type='bool', hidden=False)
         db_session.add(aggressive_path_name_shortening)
 
-        max_path_name_length = Configuration(key='max_path_name_length', value='45')
+        max_path_name_length = Configuration(key='max_path_name_length', value='45', value_type='int', hidden=False)
         db_session.add(max_path_name_length)
 
-        on_path_explode_fail_all = Configuration(key='on_path_explode_fail_all', value='False')
+        on_path_explode_fail_all = Configuration(key='on_path_explode_fail_all', value='False', value_type='bool', hidden=False)
         db_session.add(on_path_explode_fail_all)
 
-        on_path_explode_cut_course_name = Configuration(key='on_path_explode_cut_course_name', value='False')
+        on_path_explode_cut_course_name = Configuration(key='on_path_explode_cut_course_name', value='False', value_type='bool', hidden=False)
         db_session.add(on_path_explode_cut_course_name)
 
-        on_path_explode_cut_module_name = Configuration(key='on_path_explode_cut_module_name', value='True')
+        on_path_explode_cut_module_name = Configuration(key='on_path_explode_cut_module_name', value='True', value_type='bool', hidden=False)
         db_session.add(on_path_explode_cut_module_name)
 
-        on_path_explode_cut_lesson_name = Configuration(key='on_path_explode_cut_lesson_name', value='True')
+        on_path_explode_cut_lesson_name = Configuration(key='on_path_explode_cut_lesson_name', value='True', value_type='bool', hidden=False)
         db_session.add(on_path_explode_cut_lesson_name)
 
-        on_path_explode_cut_file_name = Configuration(key='on_path_explode_cut_file_name', value='True')
+        on_path_explode_cut_file_name = Configuration(key='on_path_explode_cut_file_name', value='True', value_type='bool', hidden=False)
         db_session.add(on_path_explode_cut_file_name)
 
-        on_path_explode_use_name_fallback = Configuration(key='on_path_explode_use_name_fallback', value='True')
+        on_path_explode_use_name_fallback = Configuration(key='on_path_explode_use_name_fallback', value='True', value_type='bool', hidden=False)
         db_session.add(on_path_explode_use_name_fallback)
 
-        use_custom_ffmpeg_arguments = Configuration(key='use_custom_ffmpeg_arguments', value='False')
-        db_session.add(use_custom_ffmpeg_arguments)
-
-        custom_ffmpeg_arguments = Configuration(key='custom_ffmpeg_arguments', value='-i,"{input_file}",-c:v,libx264,-crf,23,-c:a,aac,-b:a,192k,"{output_file}"')
-        db_session.add(custom_ffmpeg_arguments)
-
-        use_fast_ffmpeg_hls_conversion = Configuration(key='fast_ffmpeg_hls_conversion', value='False')
-        db_session.add(use_fast_ffmpeg_hls_conversion)
-
-        fast_ffmpeg_hls_conversion_arguments = Configuration(key='fast_ffmpeg_hls_conversion_arguments', value='-i,"{input_file}",-c,copy,"{output_file}"')
-        db_session.add(fast_ffmpeg_hls_conversion_arguments)
-
-        media_name_fallback = Configuration(key='media_name_fallback', value='{file_type}')
-        db_session.add(media_name_fallback)
-
-        enumerate_files = Configuration(key='enumerate_files', value='True')
+        enumerate_files = Configuration(key='enumerate_files', value='True', value_type='bool', hidden=False)
         db_session.add(enumerate_files)
 
-        download_subtitles = Configuration(key='download_subtitles', value='False')
+        media_name_fallback = Configuration(key='media_name_fallback', value='{file_type}', value_type='str', hidden=True)
+        db_session.add(media_name_fallback)
+
+        use_custom_ffmpeg_arguments = Configuration(key='use_custom_ffmpeg_arguments', value='False', value_type='bool', hidden=False)
+        db_session.add(use_custom_ffmpeg_arguments)
+
+        custom_ffmpeg_arguments = Configuration(key='custom_ffmpeg_arguments', value='-i,"{input_file}",-c:v,libx264,-crf,23,-c:a,aac,-b:a,192k,"{output_file}"', value_type='str', hidden=False)
+        db_session.add(custom_ffmpeg_arguments)
+
+        use_fast_ffmpeg_hls_conversion = Configuration(key='fast_ffmpeg_hls_conversion', value='False', value_type='bool', hidden=False)
+        db_session.add(use_fast_ffmpeg_hls_conversion)
+
+        fast_ffmpeg_hls_conversion_arguments = Configuration(key='fast_ffmpeg_hls_conversion_arguments', value='-i,"{input_file}",-c,copy,"{output_file}"', value_type='str', hidden=False)
+        db_session.add(fast_ffmpeg_hls_conversion_arguments)
+
+        download_subtitles = Configuration(key='download_subtitles', value='False', value_type='bool', hidden=False)
         db_session.add(download_subtitles)
 
-        subtitle_language = Configuration(key='subtitle_language', value='pt')
+        subtitle_language = Configuration(key='subtitle_language', value='pt', value_type='select', hidden=False)
         db_session.add(subtitle_language)
 
-        download_quality = Configuration(key='download_quality', value='1080p')
+        download_quality = Configuration(key='download_quality', value='1080p', value_type='select', hidden=False)
         db_session.add(download_quality)
 
-        download_quality_fallback = Configuration(key='download_quality_fallback', value='best')
+        download_quality_fallback = Configuration(key='download_quality_fallback', value='best', value_type='select', hidden=False)
         db_session.add(download_quality_fallback)
 
-        video_download_format = Configuration(key='video_download_format', value='mp4')
+        video_download_format = Configuration(key='video_download_format', value='mp4', value_type='select', hidden=False)
         db_session.add(video_download_format)
 
-        download_threads = Configuration(key='download_threads', value='1')
+        download_threads = Configuration(key='download_threads', value='1', value_type='int', hidden=True)
         db_session.add(download_threads)
 
-        stream_download_threads = Configuration(key='stream_download_threads', value='5')
+        stream_download_threads = Configuration(key='stream_download_threads', value='5', value_type='int', hidden=False)
         db_session.add(stream_download_threads)
 
-        stream_prefer_ytdlp = Configuration(key='stream_prefer_ytdlp', value='False')
+        stream_prefer_ytdlp = Configuration(key='stream_prefer_ytdlp', value='True', value_type='bool', hidden=False)
         db_session.add(stream_prefer_ytdlp)
 
-        download_maximum_retries_per_file = Configuration(key='download_maximum_retries_per_file', value='5')
+        download_maximum_retries_per_file = Configuration(key='download_maximum_retries_per_file', value='5', value_type='int', hidden=False)
         db_session.add(download_maximum_retries_per_file)
 
-        download_await_time = Configuration(key='download_await_time', value='5')
+        download_await_time = Configuration(key='download_await_time', value='5', value_type='int', hidden=False)
         db_session.add(download_await_time)
 
-        download_await_on_fail = Configuration(key='download_await_on_fail', value='5')
+        download_await_on_fail = Configuration(key='download_await_on_fail', value='5', value_type='int', hidden=False)
         db_session.add(download_await_on_fail)
 
-        reauth_on_fail = Configuration(key='reauth_on_fail', value='False')
+        reauth_on_fail = Configuration(key='reauth_on_fail', value='False', value_type='bool', hidden=False)
         db_session.add(reauth_on_fail)
 
-        reauth_fail_threshold = Configuration(key='reauth_fail_threshold', value='3')
+        reauth_fail_threshold = Configuration(key='reauth_fail_threshold', value='3', value_type='int', hidden=False)
         db_session.add(reauth_fail_threshold)
 
-        reauth_await_time = Configuration(key='reauth_await_time', value='180')
+        reauth_await_time = Configuration(key='reauth_await_time', value='180', value_type='int', hidden=False)
         db_session.add(reauth_await_time)
 
-        reauth_maximum_retries = Configuration(key='reauth_maximum_retries', value='2')
+        reauth_maximum_retries = Configuration(key='reauth_maximum_retries', value='2', value_type='int', hidden=False)
         db_session.add(reauth_maximum_retries)
 
-        download_stream_segment_in_order = Configuration(key='download_stream_segment_in_order', value='True')
+        download_stream_segment_in_order = Configuration(key='download_stream_segment_in_order', value='True', value_type='bool', hidden=False)
         db_session.add(download_stream_segment_in_order)
 
-        default_user_agent = Configuration(key='default_user_agent', value='Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:122.0) Gecko/20100101 Firefox/122.0')
+        default_user_agent = Configuration(key='default_user_agent', value='Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:122.0) Gecko/20100101 Firefox/122.0', value_type='str', hidden=False)
         db_session.add(default_user_agent)
 
-        download_from_widevine = Configuration(key='download_from_widevine', value='False')
+        download_from_widevine = Configuration(key='download_from_widevine', value='False', value_type='bool', hidden=False)
         db_session.add(download_from_widevine)
 
-        cdm_path = Configuration(key='cdm_path', value='')
+        cdm_path = Configuration(key='cdm_path', value='', value_type='str', hidden=False)
         db_session.add(cdm_path)
 
-        downloaded_videos_from_html_files = Configuration(key='downloaded_videos_from_html_files', value='False')
+        downloaded_videos_from_html_files = Configuration(key='download_videos_from_html_files', value='False', value_type='bool', hidden=False)
         db_session.add(downloaded_videos_from_html_files)
 
-        auth_threads = Configuration(key='auth_threads', value='1')
-        db_session.add(auth_threads)
-
         # API CONFIGURATION
-        allow_remote_api_communication = Configuration(key='allow_remote_api_communication', value='False')
+        allow_remote_api_communication = Configuration(key='allow_remote_api_communication', value='False', value_type='bool', hidden=False)
         db_session.add(allow_remote_api_communication)
 
         unhashed_hwid, hashed_hwid = generate_hwid()
-        public_hwid = Configuration(key='public_hwid', value=hashed_hwid)
+        public_hwid = Configuration(key='public_hwid', value=hashed_hwid, value_type='str', hidden=False)
         db_session.add(public_hwid)
-        registration_uuid = Configuration(key='registration_uuid', value=unhashed_hwid)
+        registration_uuid = Configuration(key='registration_uuid', value=unhashed_hwid, value_type='str', hidden=True)
         db_session.add(registration_uuid)
-        public_pgp_key = Configuration(key='public_pgp_key', value='Cadastrar')
+        public_pgp_key = Configuration(key='public_pgp_key', value='Cadastrar', value_type='str', hidden=False)
         db_session.add(public_pgp_key)
-        api_username = Configuration(key='api_username', value='Cadastrar')
+        api_username = Configuration(key='api_username', value='Cadastrar', value_type='str', hidden=False)
         db_session.add(api_username)
-        api_password = Configuration(key='api_password', value='Cadastrar')
+        api_password = Configuration(key='api_password', value='Cadastrar', value_type='str', hidden=False)
         db_session.add(api_password)
-        api_token = Configuration(key='api_token', value='Cadastrar')
+        api_token = Configuration(key='api_token', value='Cadastrar', value_type='str', hidden=False)
         db_session.add(api_token)
-        api_consent = Configuration(key='api_consent', value='False')
+        api_consent = Configuration(key='api_consent', value='False', value_type='bool', hidden=True)
         db_session.add(api_consent)
-        api_consent_date = Configuration(key='api_consent_date', value='0')
+        api_consent_date = Configuration(key='api_consent_date', value='0', value_type='int', hidden=True)
         db_session.add(api_consent_date)
 
         db_session.commit()
@@ -258,8 +245,8 @@ def set_download_path(download_path: pathlib.Path=pathlib.Path(SERVER_PATH),
 
     remaining = remaining_path_length(pathlib.Path(download_path), user_os)
 
-    download_conf = Configuration(key='download_path', value=download_path)
-    char_conf = Configuration(key='remaining_save_length', value=remaining)
+    download_conf = Configuration(key='download_path', value=download_path, value_type='str', hidden=False, editable=True)
+    char_conf = Configuration(key='remaining_save_length', value=remaining, value_type='int', hidden=False, editable=False)
 
     return download_conf, char_conf
 
@@ -274,8 +261,8 @@ def set_home_address(home_address: str='katomart.com',
     Returns:
         tuple[Configuration, Configuration, Configuration]: The home address, the repository URL, and the development organization
     """
-    home_conf = Configuration(key='home_address', value=home_address)
-    repo_conf = Configuration(key='repository_url', value=repo_url)
-    dev_conf = Configuration(key='development_org', value=development_org)
+    home_conf = Configuration(key='home_address', value=home_address, value_type='str', hidden=False, editable=False)
+    repo_conf = Configuration(key='repository_url', value=repo_url, value_type='str', hidden=False, editable=False)
+    dev_conf = Configuration(key='development_org', value=development_org, value_type='str', hidden=False, editable=False)
 
     return home_conf, repo_conf, dev_conf
